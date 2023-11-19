13b91848ccc3390cda193288200e2517ed8cba7a
Maurice Huang <maurice.huang@goalsmapper.com>
2023-11-19 10:40:35 +0800

    add validation for invoice creation error

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -19,7 +19,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
   return (
     <form action={dispatch}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      <div className="rounded-md bg-gray-50 p-4 md:p-6" aria-describedby='create-error'>
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -133,6 +133,12 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </p>
             ))}
         </div>
+      </div>
+      <div id="create-error" aria-live="polite" aria-atomic="true">
+        {state.message ?
+          (<p className="mt-2 text-sm text-red-500">
+            {state.message}
+          </p>) : (<></>)}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
